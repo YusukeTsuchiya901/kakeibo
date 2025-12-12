4009c87fa48aec7d924841de13e5fac71b60198e
YusukeTsuchiya901 <166134539+YusukeTsuchiya901@users.noreply.github.com>
2025-12-13 00:41:31 +0900

    feat: User Entity追加

---
+++ main/resources/main/java/com/example/MyKakeibo/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.MyKakeibo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at", insertable = false, updatable = false)
+    private Timestamp updatedAt;
+}
